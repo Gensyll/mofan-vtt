e791d0c8f532545872b97bef7c57e8f1aaf14231
Gensyll <Gensyll@users.noreply.github.com>
2025-06-20 08:35:32 -0400

    Import boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const MOFAN = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+MOFAN.abilities = {
+  str: 'MOFAN.Ability.Str.long',
+  dex: 'MOFAN.Ability.Dex.long',
+  con: 'MOFAN.Ability.Con.long',
+  int: 'MOFAN.Ability.Int.long',
+  wis: 'MOFAN.Ability.Wis.long',
+  cha: 'MOFAN.Ability.Cha.long',
+};
+
+MOFAN.abilityAbbreviations = {
+  str: 'MOFAN.Ability.Str.abbr',
+  dex: 'MOFAN.Ability.Dex.abbr',
+  con: 'MOFAN.Ability.Con.abbr',
+  int: 'MOFAN.Ability.Int.abbr',
+  wis: 'MOFAN.Ability.Wis.abbr',
+  cha: 'MOFAN.Ability.Cha.abbr',
+};
