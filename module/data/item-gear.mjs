3dcc7b0968febed0d62922b62cbcf285614b403c
Gensyll <Gensyll@users.noreply.github.com>
2025-11-20 22:29:23 -0500

    Added BaseItemLootable class
    Should help manage items that are specifically lootables vs non-lootables (spells/feats/etc)

--- module/data/item-gear.mjs
+++ module/data/item-gear.mjs
@@ -1,6 +1,6 @@
-import MofanItemBase from './base-item.mjs';
+import MofanItemLootable from './base-item-lootable.mjs';
 
-export default class MofanGear extends MofanItemBase {
+export default class MofanGear extends MofanItemLootable {
   static LOCALIZATION_PREFIXES = [
     'MOFAN.Item.base',
     'MOFAN.Item.Gear',
@@ -10,23 +10,6 @@ export default class MofanGear extends MofanItemBase {
     const fields = foundry.data.fields;
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
-
-    schema.quantity = new fields.NumberField({
-      ...requiredInteger,
-      initial: 1,
-      min: 1,
-    });
-    schema.weight = new fields.NumberField({
-      required: true,
-      nullable: false,
-      initial: 0,
-      min: 0,
-    });
-    schema.size = new fields.StringField({
-      required: true,
-      initial: "small",
-      choices: ["tiny","small","medium","large","huge"]
-    });
 
     //TODO: refactor D20 rules
     // Break down roll formula into three independent fields
--- module/data/item-loot.mjs
+++ module/data/item-loot.mjs
@@ -1,6 +1,6 @@
-import MofanItemBase from './base-item.mjs';
+import MofanItemLootable from './base-item-lootable.mjs';
 
-export default class MofanLoot extends MofanItemBase {
+export default class MofanLoot extends MofanItemLootable {
     static LOCALIZATION_PREFIXES = [
         'MOFAN.Item.base',
         'MOFAN.Item.Loot',
@@ -10,24 +10,6 @@ export default class MofanLoot extends MofanItemBase {
         const fields = foundry.data.fields;
         const requiredInteger = { required: true, nullable: false, integer: true };
         const schema = super.defineSchema();
-
-        schema.quantity = new fields.NumberField({
-            ...requiredInteger,
-            initial: 1,
-            min: 1,
-        });
-        schema.weight = new fields.NumberField({
-            required: true,
-            nullable: false,
-            initial: 0,
-            min: 0,
-        });
-
-        schema.size = new fields.StringField({
-            required: true,
-            initial: "small",
-            choices: ["tiny","small","medium","large","huge"]
-        });
 
         /*schema.roll = new fields.SchemaField({
             diceNum: new fields.NumberField({
